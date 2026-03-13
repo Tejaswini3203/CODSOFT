d385e312dfab83d373785914fa6bb8dc5e450d29
Tejaswini Reddy Maram <tejaswinimaram9949@gmail.com>
2026-03-13 22:38:29 +0530

    chatbot.py

--- Task1_Chatbot/chatbot.py
+++ Task1_Chatbot/chatbot.py
@@ -5,13 +5,29 @@ while True:
     user = input("You: ").lower()
 
     if user == "hello":
-        print("Bot: Hello! How can I help you?")
+        print("Bot: Hello! Nice to meet you.")
+        
     elif user == "how are you":
-        print("Bot: I am fine. Thank you!")
+        print("Bot: I am doing great. How about you?")
+        
     elif user == "your name":
-        print("Bot: I am CodSoft Chatbot.")
+        print("Bot: My name is CodSoft Chatbot.")
+        
+    elif user == "what is ai":
+        print("Bot: AI stands for Artificial Intelligence. It allows machines to learn and make decisions.")
+        
+    elif user == "who created you":
+        print("Bot: I was created by Tejaswini for the CodSoft AI Internship.")
+        
+    elif user == "what can you do":
+        print("Bot: I can answer simple questions and chat with you.")
+        
+    elif user == "thank you":
+        print("Bot: You're welcome!")
+        
     elif user == "bye":
-        print("Bot: Goodbye!")
+        print("Bot: Goodbye! Have a nice day.")
         break
+        
     else:
-        print("Bot: Sorry, I didn't understand.")
+        print("Bot: Sorry, I didn't understand that.")
