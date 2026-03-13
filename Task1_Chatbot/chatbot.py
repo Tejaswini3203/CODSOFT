cb9e2ca95d8da3ae569b40196c0561b94f0eaca4
Tejaswini Reddy Maram <tejaswinimaram9949@gmail.com>
2026-03-13 20:55:59 +0530

    chatbot.py

--- Task1_Chatbot/chatbot.py
+++ Task1_Chatbot/chatbot.py
@@ -1,1 +1,17 @@
+print("Hello! I am a simple chatbot.")
+print("Type 'bye' to exit.")
 
+while True:
+    user = input("You: ").lower()
+
+    if user == "hello":
+        print("Bot: Hello! How can I help you?")
+    elif user == "how are you":
+        print("Bot: I am fine. Thank you!")
+    elif user == "your name":
+        print("Bot: I am CodSoft Chatbot.")
+    elif user == "bye":
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: Sorry, I didn't understand.")
